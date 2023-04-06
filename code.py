2cbd6673a7f575231ef29b5114d801e93939c50f
Abby Wadsworth <77417267+abbywadsworth@users.noreply.github.com>
2023-04-06 13:38:08 -0600

    Create code.py

---
+++ code.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+url = 'https://github.com/esnt/Data/raw/main/Names/popular_names.csv'
